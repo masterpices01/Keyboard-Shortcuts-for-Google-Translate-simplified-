243f4c559f0a0e965ee821506df4bc30277f59e1
masterpices01 <67574628+masterpices01@users.noreply.github.com>
2023-06-10 15:57:16 +0800

    when I buy a new computer I'll came here to copy these codes

--- Keyboard-Shortcuts-for-Google-Translate.js
+++ Keyboard-Shortcuts-for-Google-Translate.js
@@ -4,16 +4,22 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://translate.google.com.tw/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com.tw
+// @match        https://translate.google.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
 // @grant        none
 // ==/UserScript==
 
-
 (function() {
     'use strict';
-    // Your code here...
 
+    // Your code here...
+    //WARNING!!! YOU SHOULD CHANGE YOUR BROWSER LANGUAGE TO ENGLISH BEFORE YOU APPLY THIS SCRIPTS, CAUSE IT ONLY CAN CATCH THE TEXT "Source text".
+    //if your browser display language is not english, you should change the "aria-label" attribute to match the corresponding text. find it by dev tools inspect function.
+    //auto-focus on the text-field when you open a new window.
+    document.querySelector('[aria-label="Source text"]').focus();
+    document.querySelector('[aria-label="Source text"]').select();
+
+    //disable default hotkeys about alt
     document.addEventListener('keydown',
                               function(ev){
         //I just can't stop alt-F4 and CTRL-W though, think they are overrides you can't change, but for ones you can change, this works
@@ -47,4 +53,15 @@
 
     /*Use Alt + hotkey to listen what you typed or cleard what you've typed,
     Hope this snippet program will help your learning a new langauge with google translation. */
+
+    /*below is the css filter I used to apply on the extension: u block orign to elimiate the source text side, I only remain the definition side and read the words' definition in english.
+    you can download them in here: https://ublockorigin.com
+    */
+    /*and also recommend you choose a fixed build of chromium browser them set's default search to google translate.then set the defalt
+    search page to google chrome, here's the url I used to set:
+    https://translate.google.com/?source=en&sl=en&tl=de&op=translate&text=%s
+    */
+
+
+
 })();
